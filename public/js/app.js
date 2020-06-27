b0d048f6f7918bfcbc1d6ba1e972b95061e65311
YassenRabie <YassenRabie@users.noreply.github.com>
2020-06-27 21:56:54 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log('Clint side js is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#messege-1')
+const messageTwo = document.querySelector('#messege-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading!'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return messageOne.textContent = data.error
+            }
+
+            messageTwo.textContent = data.location
+            messageOne.textContent = data.forecast.description + ', The temp is : ' + data.forecast.temp
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=metric&exclude=hourly,daily&appid=acab20c54df52f149ac7ecd06aa05349'
+
+    request({ url, json: true }, (error, { body }) => {
+
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.cod) {
+            callback('Unable to find location!', undefined)
+        } else {
+            callback(undefined, {
+                description: body.current.weather[0].description,
+                temp: body.current.temp,
+            })
+        }
+    })
+}
+
+module.exports = forecast
